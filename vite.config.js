7fe99b107d25103534bcc75018068070e08d9db5
suminhong <honglab97@gmail.com>
2025-05-08 02:21:59 +0900

    port rollback

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 80,
+    port: 8080,
     host: true,
     allowedHosts: true,
     strictPort: true,
