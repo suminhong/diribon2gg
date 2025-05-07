95c72b83c2f237796574a477d417f4d75e4af690
suminhong <honglab97@gmail.com>
2025-05-08 02:20:14 +0900

    fix port to 80

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 8080,
+    port: 80,
     host: true,
     allowedHosts: true,
     strictPort: true,
